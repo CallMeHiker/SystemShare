2a1d6ae8eb7a9daea5bb5d9df31d00dafb01d756
Hiker <867690241@qq.com>
2018-12-05 15:34:29 +0800

    创建项目

---
+++ SystemShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SystemShare
+//
+//  Created by 吴卓荣 on 2018/12/5.
+//  Copyright © 2018年 Qcloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
